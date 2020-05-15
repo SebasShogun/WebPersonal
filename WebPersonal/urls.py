0b41e2d191d94ec0071d786793bf0f7cb33bd3f2
SebasShogun <shogun31sff@gmail.com>
2020-05-15 15:17:22 -0500

    Portafolio y Contacto

--- WebPersonal/urls.py
+++ WebPersonal/urls.py
@@ -20,5 +20,7 @@ from core import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about-me/', views.about, name='about'),
+    path('portfolio/', views.portfolio, name='portfolio'),
+    path('contact/', views.contact, name='contact'),
     path('admin/', admin.site.urls),
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,36 @@
 from django.shortcuts import render, HttpResponse
 
+html_base = """
+<h1>Mi web personal</h1>
+<ul> 
+    <li><a href="/">Portada</a></li>
+    <li><a href="/about-me">Acerca de</a></li>
+    <li><a href="/portfolio">Portafolio</a></li>
+    <li><a href="/contact">Contacto</a></li>
+</ul>
+"""
+
 # Create your views here.
 def home(request): 
-    return HttpResponse("<h1>Mi web personal</h1><h2>Portada</h2>")
+    return HttpResponse(html_base + """
+        <h2>Portada</h2>
+        <p>Esto es la portada</p>
+    """)
 
 def about(request):
-    return HttpResponse("<h1>Mi web personal</h1><h2>Acerca de</h2><p> Me llamo Sebastián y soy un programador.</p>")
+    return HttpResponse(html_base + """
+        <h2>Acerca de</h2>
+        <p> Me llamo Sebastián y soy un programador.</p>
+    """)
+
+def portfolio(request): 
+    return HttpResponse(html_base + """
+        <h2>Portafolio</h2>
+        <p>Algunos de mis trabajos</p>
+    """)
+
+def contact(request):
+    return HttpResponse(html_base + """
+        <h2>Contact</h2>
+        <p> Aqui dejo mi email para preguntarme </p>
+    """)
