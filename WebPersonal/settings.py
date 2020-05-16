7781d251dd45057ca870b77a25f9cc10e83cb62d
SebasShogun <shogun31sff@gmail.com>
2020-05-15 19:21:20 -0500

    Personalización del panel de administrador

--- WebPersonal/settings.py
+++ WebPersonal/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'portfolio',
+    'portfolio.apps.PortfolioConfig',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Media Files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
--- portfolio/admin.py
+++ portfolio/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import Project
 
 # Register your models here.
-admin.site.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+
+
+admin.site.register(Project, ProjectAdmin)
